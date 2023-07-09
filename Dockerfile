a63ce1531f5f617f2a0be796022facade9ce454d
Steve Gindi <steve.gindi@datagen.tech>
2023-07-10 00:39:07 +0300

    change stuff

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,13 @@ FROM maven:3.8.2-openjdk-11-slim AS builder
 WORKDIR /app
 COPY ./myapp /app
 ARG revision
+RUN echo $revision
 RUN mvn -B package -Drevision=$revision
 
 # Second stage: Java runner
 FROM adoptopenjdk:11-jre-hotspot
 ARG revision
-RUN echo $revision
 ENV VERSION=$revision
+RUN echo "VERSION set to: $VERSION"
 COPY --from=builder /app/target/myapp-*.jar /my-app.jar
-CMD ["java", "-jar", "/my-app.jar"]
+CMD ["sh", "-c", "java -Dversion=$VERSION -jar /my-app.jar"]
--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -1,19 +1,8 @@
 package com.myapp;
 
-import java.io.IOException;
-import java.util.jar.Manifest;
-
 public class App {
     public static void main(String[] args) {
-        String version = null;
-        try {
-            // Get the manifest file from the current JAR
-            Manifest manifest = new Manifest(App.class.getResourceAsStream("/META-INF/MANIFEST.MF"));
-            // Get the Implementation-Version attribute
-            version = manifest.getMainAttributes().getValue("Implementation-Version");
-        } catch (IOException e) {
-            // Handle exception
-        }
+        String version = System.getProperty("version");
         System.out.println("Hello World! This is Yishayahu, This is build version " + version);
     }
 }
